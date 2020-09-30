2e52f8ab6f6e6068ea5faec3196ff83be775a73a
Max_M <omen5865@gmail.com>
2020-09-30 20:15:18 +0300

    Update

---
+++ Anagram/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Anagram
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputStr = Console.ReadLine();
+            Console.WriteLine(ReverseString(inputStr));
+        }
+
+        public static string ReverseString(string inputStr)
+        {
+            string[] splitedWords = inputStr.Split(" ");
+            List<string> reversedWords = new List<string>();
+
+            for (int i = 0; i < splitedWords.Length; i++)
+            {
+                char[] charsArray = splitedWords[i].ToCharArray();
+
+                int r = charsArray.Length - 1;
+                int l = 0;
+                while (l < r)
+                {
+                    if (!char.IsLetter(charsArray[l]))
+                        l++;
+                    else if (!char.IsLetter(charsArray[r]))
+                        r--;
+                    else
+                    {
+                        char tmp = charsArray[l];
+                        charsArray[l] = charsArray[r];
+                        charsArray[r] = tmp;
+                        l++;
+                        r--;
+                    }
+                }
+                reversedWords.Add(new string(charsArray));
+            }
+            var reversedString = string.Join(" ", reversedWords);
+
+            return reversedString;
+        }
+    }
+}
